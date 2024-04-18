40b3c40d21e9be819abf24fab1fd349dbc548c08
Cassio Pieroni <cassiopieroni@gmail.com>
2024-04-18 14:47:33 -0300

    test: improve status endpoint

--- pages/api/v1/status/index.test.js
+++ pages/api/v1/status/index.test.js
@@ -4,5 +4,14 @@ describe(`/api/v1/status`, () => {
   test(`GET responds with 200 status`, async () => {
     const response = await fetch(`${url_base}/api/v1/status`);
     expect(response.status).toBe(200);
+
+    const { updated_at, dependencies: { database } } = await response.json(); 
+
+    const parsedUpdatedAt = new Date(updated_at).toISOString();
+    expect(updated_at).toEqual(parsedUpdatedAt);
+
+    expect(database.version).toEqual('16.0');
+    expect(database.max_connections).toEqual(100);
+    expect(database.opened_connections).toEqual(1);
   });
 })
