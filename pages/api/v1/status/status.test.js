909cbfe10cd603f30ea61ace3503d8ae6cb00c2e
Cassio Pieroni <cassiopieroni@gmail.com>
2024-05-03 17:29:14 -0300

    refactor(`/status`): improve test

--- pages/api/v1/status/status.test.js
+++ pages/api/v1/status/status.test.js
@@ -1,17 +1,18 @@
 const url_base = `http://localhost:3000`;
 
 describe(`/api/v1/status`, () => {
-  test(`GET responds with 200 status`, async () => {
-    const response = await fetch(`${url_base}/api/v1/status`);
-    expect(response.status).toBe(200);
+  describe(`GET`, () => {
+    test(`GET responds with 200 status`, async () => {
+      const response = await fetch(`${url_base}/api/v1/status`);
+      expect(response.status).toBe(200);
 
-    const { updated_at, dependencies: { database } } = await response.json(); 
+      const { updated_at, dependencies: { database } } = await response.json(); 
+      const parsedUpdatedAt = new Date(updated_at).toISOString();
 
-    const parsedUpdatedAt = new Date(updated_at).toISOString();
-    expect(updated_at).toEqual(parsedUpdatedAt);
-
-    expect(database.version).toEqual('16.0');
-    expect(database.max_connections).toEqual(100);
-    expect(database.opened_connections).toEqual(1);
+      expect(updated_at).toEqual(parsedUpdatedAt);
+      expect(database.version).toEqual('16.0');
+      expect(database.max_connections).toEqual(100);
+      expect(database.opened_connections).toEqual(1);
+    });
   });
 })
